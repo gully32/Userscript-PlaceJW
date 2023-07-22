e445cac4a3bec91b406703dd988be30ddb74c4e0
gully32 <34196438+gully32@users.noreply.github.com>
2023-07-22 21:43:54 +0100

    Update constants.js
    changed url from PlaceNL to PlaceJW

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'wss://chief.placenl.nl/ws';
+export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'wss://placejw.jumpwire.dev/ws';
 export const USERSCRIPT_REVISION = ___GIT_HASH___ || 'development';
