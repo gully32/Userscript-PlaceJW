47c857138df50322e563a69c7462152295e8c138
gully32 <34196438+gully32@users.noreply.github.com>
2023-07-22 22:36:49 +0100

    Update placejw-userscript-autoupdater.user.js

--- placejw-userscript-autoupdater.user.js
+++ placejw-userscript-autoupdater.user.js
@@ -9,13 +9,13 @@
 // @connect      reddit.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @updateURL    https://github.com/gully32/Userscript-PlaceJW/releases/download/latest/placenl-userscript-autoupdater.user.js
-// @downloadURL  https://github.com/gully32/Userscript-PlaceJW/releases/download/latest/placenl-userscript.user.js
+// @downloadURL  https://github.com/gully32/Userscript-PlaceJW/releases/download/latest/placenl-userscript-autoupdater.user.js
 // @grant        GM.xmlHttpRequest
 // @connect      github.com
 // @connect      objects.githubusercontent.com
 // ==/UserScript==
 
-const SCRIPT_LOCATION = 'https://github.com/gully32/Userscript-PlaceJW/releases/download/latest/placenl-userscript-autoupdater.user.js';
+const SCRIPT_LOCATION = 'https://github.com/gully32/Userscript-PlaceJW/releases/download/latest/placenl-userscript.user.js';
 const UPDATE_CHECK_INTERVAL = 10 * 60 * 1000;
 
 (function () {
